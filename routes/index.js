31fc42b8fcc1698176d3850a7ec87d6e9cb1c64e
Krishnadev P Melevila <krishnadev@kali>
2022-05-31 19:36:58 +0530

    Link feature test

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var axios = require("axios").default;
+var validUrl = require('valid-url');
 const { body, checkSchema, validationResult } = require('express-validator');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {layout: 'default', template: 'home-template',title:'Youtube Downloader'});
+router.get('/', function (req, res, next) {
+  res.render('index', { layout: 'default', template: 'home-template', title: 'Youtube Downloader' });
 });
 
 
-router.post('/downloadmp3',function(req,res){
-  
+router.post('/downloadmp3', function (req, res) {
+
   var options = {
     method: 'GET',
     url: 'https://youtube-mp36.p.rapidapi.com/dl',
-    params: {id: req.body.mus},
+    params: { id: req.body.mus },
     headers: {
       'x-rapidapi-host': 'youtube-mp36.p.rapidapi.com',
       'x-rapidapi-key': '149496b202msh3bc1ce64534abfbp12c6c3jsn0da37c1922b6'
@@ -26,12 +27,12 @@ router.post('/downloadmp3',function(req,res){
     console.error(error);
   });
 })
-router.post('/downloadmp4',function(req,res){
+router.post('/downloadmp4', function (req, res) {
   console.log(req.body.vid);
   var options = {
     method: 'GET',
     url: 'https://ytstream-download-youtube-videos.p.rapidapi.com/dl',
-    params: {id: req.body.vid},
+    params: { id: req.body.vid },
     headers: {
       'x-rapidapi-host': 'ytstream-download-youtube-videos.p.rapidapi.com',
       'x-rapidapi-key': '149496b202msh3bc1ce64534abfbp12c6c3jsn0da37c1922b6'
@@ -43,51 +44,87 @@ router.post('/downloadmp4',function(req,res){
     console.error(error);
   });
 })
-const searchSchema={
-  search:{
-    notEmpty:true,
-    errorMessage:"Hey! Please search something..."
+const searchSchema = {
+  search: {
+    notEmpty: true,
+    errorMessage: "Hey! Please search something..."
   }
 }
 const validate = validations => {
   return async (req, res, next) => {
-    if (req.body.search.length>120){
+    if (req.body.search.length > 120) {
       res.status(400)
-      return res.render('index',{layout: 'default', template: 'home-template',error:"You tried to fill our database! Please try to enter some short Keyword!",title:'Youtube Downloader'})
+      return res.render('index', { layout: 'default', template: 'home-template', error: "You tried to fill our database! Please try to enter some short Keyword!", title: 'Youtube Downloader' })
     }
-      await Promise.all(validations.map(validation => validation.run(req)));
+    await Promise.all(validations.map(validation => validation.run(req)));
 
-      const errors = validationResult(req);
-      if (errors.isEmpty()) {
-          return next();
-      }
-      
-      res.status(400)
-      return res.render('index',{layout: 'default', template: 'home-template',error:"Hey! Please search something...",title:'Youtube Downloader'})
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    res.status(400)
+    return res.render('index', { layout: 'default', template: 'home-template', error: "Hey! Please search something...", title: 'Youtube Downloader' })
   };
 };
-router.post('/',validate(checkSchema(searchSchema)),(req,res)=>{
-  
+router.post('/', validate(checkSchema(searchSchema)), (req, res) => {
+  var url = req.body.search
+  if (validUrl.isUri(url)) {
+    const axios = require("axios");
+
+    const options = {
+      method: 'GET',
+      url: 'https://simple-youtube-search.p.rapidapi.com/video',
+      params: { search: url },
+      headers: {
+        'X-RapidAPI-Host': 'simple-youtube-search.p.rapidapi.com',
+        'X-RapidAPI-Key': 'ab7de61726mshd2142f88fc2a5bbp18445djsnd7504c3cfd38'
+      }
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data.result.title);
+
+      var options = {
+        method: 'GET',
+        url: 'https://simple-youtube-search.p.rapidapi.com/search',
+        params: { query: response.data.result.title, type: 'video', safesearch: 'true' },
+        headers: {
+          'x-rapidapi-host': 'simple-youtube-search.p.rapidapi.com',
+          'x-rapidapi-key': '149496b202msh3bc1ce64534abfbp12c6c3jsn0da37c1922b6'
+        }
+      };
+      axios.request(options).then(function (response) {
+        console.log(response.data.results);
+        res.render('index', { layout: 'default', template: 'home-template', title: 'Youtube Downloader', 'data': response.data.results });
+      }).catch(function (error) {
+        return res.render('index', { layout: 'default', template: 'home-template', error: "Sorry, Youtube doesn't have what you searched for.", title: 'Youtube Downloader' })
+      });
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
+  else {
+    var options = {
+      method: 'GET',
+      url: 'https://simple-youtube-search.p.rapidapi.com/search',
+      params: { query: req.body.search, type: 'video', safesearch: 'true' },
+      headers: {
+        'x-rapidapi-host': 'simple-youtube-search.p.rapidapi.com',
+        'x-rapidapi-key': '149496b202msh3bc1ce64534abfbp12c6c3jsn0da37c1922b6'
+      }
+    };
+    axios.request(options).then(function (response) {
+      console.log(response.data.results);
+      res.render('index', { layout: 'default', template: 'home-template', title: 'Youtube Downloader', 'data': response.data.results });
+    }).catch(function (error) {
+      return res.render('index', { layout: 'default', template: 'home-template', error: "Sorry, Youtube doesn't have what you searched for.", title: 'Youtube Downloader' })
+    });
+  }
 
 
-  var options = {
-    method: 'GET',
-    url: 'https://simple-youtube-search.p.rapidapi.com/search',
-    params: {query: req.body.search, type: 'video', safesearch: 'true'},
-    headers: {
-      'x-rapidapi-host': 'simple-youtube-search.p.rapidapi.com',
-      'x-rapidapi-key': '149496b202msh3bc1ce64534abfbp12c6c3jsn0da37c1922b6'
-    }
-  };
 
-  axios.request(options).then(function (response) {
-    res.render('index',{layout: 'default', template: 'home-template',title:'Youtube Downloader','data':response.data.results});
-  }).catch(function (error) {
-    return res.render('index',{layout: 'default', template: 'home-template',error:"Sorry, Youtube doesn't have what you searched for.",title:'Youtube Downloader'})
-  });
-  // res.send({
-  //   data:req.body.search
-  // })
+
 })
 
 module.exports = router;
